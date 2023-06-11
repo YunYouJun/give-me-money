d8a0587e6a0888f480daa3a9cafb88fb3200e45e
YunYouJun <YunYouJun@users.noreply.github.com>
2023-06-11 04:26:37 +0000

    deploy: 2f6fb0ebc887792fbd3aaf4d4cc48fab490a5d5c

---
+++ assets/_...all_-bfdaf09b.js
@@ -0,0 +1,1 @@
+import{d as o,a as e,o as a,c as s,t as c,f as r}from"./app-a4e2cad9.js";const f=o({__name:"[...all]",setup(l){const{t}=e();return(p,u)=>(a(),s("div",null,c(r(t)("not-found")),1))}}),n={};typeof n=="function"&&n(f);export{f as default};
